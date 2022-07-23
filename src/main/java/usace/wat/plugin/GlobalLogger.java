c31fea9ee41ee94a82f7d135e1ba12adffa08a7e
Will Lehman <william.p.lehman@usace.army.mil>
2022-07-23 14:35:56 -0400

    starting the logger

---
+++ src/main/java/usace/wat/plugin/GlobalLogger.java
@@ -0,0 +1,9 @@
+package usace.wat.plugin;
+import com.fasterxml.jackson.annotation.JsonProperty;
+public static final GlobalLogger {
+    public static void Log(message Message){
+        //
+    }
+
+}
+
