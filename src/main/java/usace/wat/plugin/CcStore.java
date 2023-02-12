e3ef4b31aee4d1e03cc0f493589edd080728afce
Will Lehman <william.p.lehman@usace.army.mil>
2023-02-12 09:19:56 -0500

    pullobjectinput

--- src/main/java/usace/wat/plugin/CcStore.java
+++ src/main/java/usace/wat/plugin/CcStore.java
@@ -1,5 +1,13 @@
 package usace.wat.plugin;
 
-public class CcStore {
-    
+import java.rmi.RemoteException;
+
+public interface CcStore {
+    public boolean PutObject(PutObjectInput input);
+    public boolean PullObject(PullObjectInput input);
+    public byte[] GetObject(GetObjectInput input) throws RemoteException;
+    public Payload GetPayload() throws RemoteException;
+    //public void SetPayload(Payload payload); only used in the go sdk to support cloudcompute which is written in go.
+    public String RootPath();
+    public boolean HandlesDataStoreType(StoreType datastoretype);
 }
--- src/main/java/usace/wat/plugin/PullObjectInput.java
+++ src/main/java/usace/wat/plugin/PullObjectInput.java
@@ -1,5 +1,37 @@
 package usace.wat.plugin;
 
-public class PullObjectInput {
+import java.io.FilenameFilter;
 
+public class PullObjectInput {
+    private String fileName;
+    private String fileExtension;
+    private StoreType sourceStoreType;
+    private String sourceRootPath;
+    private String destRootPath;
+    public String getFileName(){
+        return fileName;
+    }
+    public String getFileExtension(){
+        return fileExtension;
+    }
+    public StoreType getSourceStoreType(){
+        return sourceStoreType;
+    }
+    public String getSourceRootPath(){
+        return sourceRootPath;
+    }
+    public String getDestRootPath(){
+        return destRootPath;
+    }
+    /**
+     * 
+     */
+    public PullObjectInput(String fileName, StoreType sourceStoreType, String sourceRootPath, String destRootPath, String fileExtension){
+        this.fileName = fileName;
+        this.sourceStoreType = sourceStoreType;
+        this.sourceRootPath = sourceRootPath;
+        this.destRootPath = destRootPath;
+        this.fileExtension = fileExtension;
+    }
 }
+
