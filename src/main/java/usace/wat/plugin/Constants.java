2b5c91ea95ca047d6970b871bed036fc9befd7f6
Will Lehman <william.p.lehman@usace.army.mil>
2023-02-24 08:36:09 -0500

    moving remoterootpath for testing

--- src/main/java/usace/wat/plugin/Constants.java
+++ src/main/java/usace/wat/plugin/Constants.java
@@ -3,5 +3,5 @@ package usace.wat.plugin;
 public final class Constants {
     public static String PayloadFileName = "payload";
     public static String LocalRootPath = "/data";
-    public static String RemoteRootPath = "cc_store";
+    public static String RemoteRootPath = "model-library/Fema FFRD Test/Muncie/cc_store";
 }
