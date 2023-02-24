fa9b572eb3bf4a450b8022b062c2d61b2cb6470b
Will Lehman <william.p.lehman@usace.army.mil>
2023-02-23 20:51:54 -0500

    setting config before accessing config

--- src/main/java/usace/wat/plugin/FileDataStoreS3.java
+++ src/main/java/usace/wat/plugin/FileDataStoreS3.java
@@ -95,11 +95,12 @@ public class FileDataStoreS3 implements FileDataStore {
         //acfg.aws_endpoint = System.getenv("S3_ENDPOINT");
         //acfg.aws_disable_ssl = Boolean.parseBoolean(System.getenv("S3_DISABLE_SSL"));//convert to bool?
         //acfg.aws_force_path_style = Boolean.parseBoolean(System.getenv("S3_FORCE_PATH_STYLE"));//convert to bool
+        config = acfg;
         System.out.println(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_DEFAULT_REGION+"::"+config.aws_region);
         System.out.println(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_ACCESS_KEY_ID+"::"+config.aws_access_key_id);
         System.out.println(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_SECRET_ACCESS_KEY+"::"+config.aws_secret_access_key_id);
         System.out.println(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_S3_BUCKET+"::"+config.aws_bucket);
-        config = acfg;
+        
         Regions clientRegion = Regions.valueOf(config.aws_region.toUpperCase().replace("-", "_"));
         try {
             AmazonS3 s3Client = null;
