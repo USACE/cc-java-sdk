acbe331c4d2cbefccb014884720fc57f37869de2
Andrew Song <andrewsong.j@gmail.com>
2023-08-11 17:26:12 -0400

    error check for null environmental variables

--- src/main/java/usace/cc/plugin/CcStoreS3.java
+++ src/main/java/usace/cc/plugin/CcStoreS3.java
@@ -42,12 +42,26 @@ public class CcStoreS3 implements CcStore {
     public CcStoreS3(){
         AWSConfig acfg = new AWSConfig();
         acfg.aws_access_key_id = System.getenv(EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_ACCESS_KEY_ID);
+        if(acfg.aws_access_key_id == null)
+            System.out.println("ERROR: `" + EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_ACCESS_KEY_ID + "` environment variable is not set.");
         acfg.aws_secret_access_key_id = System.getenv(EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_SECRET_ACCESS_KEY);
+        if(acfg.aws_secret_access_key_id == null)
+            System.out.println("ERROR: `" + EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_SECRET_ACCESS_KEY + "` environment variable is not set.");
         acfg.aws_region = System.getenv(EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_DEFAULT_REGION);
+        if(acfg.aws_region == null)
+            System.out.println("ERROR: `" + EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_DEFAULT_REGION + "` environment variable is not set.");
         acfg.aws_bucket = System.getenv(EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_S3_BUCKET);
+        if(acfg.aws_bucket == null)
+            System.out.println("ERROR: `" + EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_S3_BUCKET + "` environment variable is not set.");
+        if(System.getenv(EnvironmentVariables.CC_PROFILE + "_" +"S3_MOCK") == null)
+            System.out.println("WARNING: `" + EnvironmentVariables.CC_PROFILE + "_" + "S3_MOCK" + "` environment variable is not set. Defaulted to false");
         acfg.aws_mock = Boolean.parseBoolean(System.getenv(EnvironmentVariables.CC_PROFILE + "_" +"S3_MOCK"));//convert to boolean;//stringformat
         acfg.aws_endpoint = System.getenv(EnvironmentVariables.CC_PROFILE + "_" +"S3_ENDPOINT");
+        if(acfg.aws_mock == true && acfg.aws_endpoint == null)
+            System.out.println("ERROR: `" + EnvironmentVariables.CC_PROFILE + "_" + "S3_ENDPOINT" + "` environment variable is not set even though using mock S3.");
         acfg.aws_disable_ssl = Boolean.parseBoolean(System.getenv(EnvironmentVariables.CC_PROFILE + "_" +"S3_DISABLE_SSL"));//convert to bool?
+        if(acfg.aws_mock == true && System.getenv(EnvironmentVariables.CC_PROFILE + "_" +"S3_FORCE_PATH_STYLE") == null)
+            System.out.println("WARNING: `" + EnvironmentVariables.CC_PROFILE + "_" + "S3_FORCE_PATH_STYLE" + "` environment variable is not set even though using mock S3--defaulted to false.");
         acfg.aws_force_path_style = Boolean.parseBoolean(System.getenv(EnvironmentVariables.CC_PROFILE + "_" +"S3_FORCE_PATH_STYLE"));//convert to bool
         config = acfg;
         //System.out.println(EnvironmentVariables.CC_PROFILE + "_" + EnvironmentVariables.AWS_DEFAULT_REGION+"::"+config.aws_region);
--- src/main/java/usace/cc/plugin/FileDataStoreS3.java
+++ src/main/java/usace/cc/plugin/FileDataStoreS3.java
@@ -88,12 +88,26 @@ public class FileDataStoreS3 implements FileDataStore {
     public FileDataStoreS3(DataStore ds){
         AWSConfig acfg = new AWSConfig();
         acfg.aws_access_key_id = System.getenv(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_ACCESS_KEY_ID);
+        if(acfg.aws_access_key_id == null)
+            System.out.println("ERROR: `" + ds.getDsProfile() + "_" + EnvironmentVariables.AWS_ACCESS_KEY_ID + "` environment variable is not set.");
         acfg.aws_secret_access_key_id = System.getenv(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_SECRET_ACCESS_KEY);
+        if(acfg.aws_secret_access_key_id == null)
+            System.out.println("ERROR: `" + ds.getDsProfile() + "_" + EnvironmentVariables.AWS_SECRET_ACCESS_KEY + "` environment variable is not set.");
         acfg.aws_region = System.getenv(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_DEFAULT_REGION);
+        if(acfg.aws_region == null)
+            System.out.println("ERROR: `" + ds.getDsProfile() + "_" + EnvironmentVariables.AWS_DEFAULT_REGION + "` environment variable is not set.");
         acfg.aws_bucket = System.getenv(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_S3_BUCKET);
+        if(acfg.aws_bucket == null)
+            System.out.println("ERROR: `" + ds.getDsProfile() + "_" + EnvironmentVariables.AWS_S3_BUCKET + "` environment variable is not set.");
+        if(System.getenv(ds.getDsProfile() + "_" +"S3_MOCK") == null)
+            System.out.println("WARNING: `" + ds.getDsProfile() + "_" + "S3_MOCK" + "` environment variable is not set. Defaulted to false");
         acfg.aws_mock = Boolean.parseBoolean(System.getenv(ds.getDsProfile() + "_"+ "S3_MOCK"));//convert to boolean;
         acfg.aws_endpoint = System.getenv(ds.getDsProfile() + "_"+ "S3_ENDPOINT");
-        acfg.aws_disable_ssl = Boolean.parseBoolean(System.getenv(ds.getDsProfile() + "_"+ "S3_DISABLE_SSL"));//convert to bool?
+        if(acfg.aws_mock == true && acfg.aws_endpoint == null)
+            System.out.println("ERROR: `" + ds.getDsProfile() + "_" + "S3_ENDPOINT" + "` environment variable is not set even though using mock S3.");
+        acfg.aws_disable_ssl = Boolean.parseBoolean(System.getenv(ds.getDsProfile() + "_"+ "S3_DISABLE_SSL"));//convert to bool? //Never used
+        if(acfg.aws_mock == true && System.getenv(ds.getDsProfile() + "_" +"S3_FORCE_PATH_STYLE") == null)
+            System.out.println("WARNING: `" + ds.getDsProfile() + "_" + "S3_FORCE_PATH_STYLE" + "` environment variable is not set even though using mock S3--defaulted to false.");
         acfg.aws_force_path_style = Boolean.parseBoolean(System.getenv(ds.getDsProfile() + "_"+ "S3_FORCE_PATH_STYLE"));//convert to bool
         config = acfg;
         //System.out.println(ds.getDsProfile() + "_" + EnvironmentVariables.AWS_DEFAULT_REGION+"::"+config.aws_region);
